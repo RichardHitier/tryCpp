52059da5c5dd417f356ce4469911087f39ea7e7a
Richard Hitier <richard.hitier@co-libri.org>
2015-09-08 10:56:29 +0200

    function shows types lengths

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+void showtypes();
+
 main(){
-    cout << "hello world\n";
+    int x, y;
+    showtypes();
     return 0;
+}
+
+void showtypes(){
+    cout << "Size of char : " << sizeof(char) << endl;
+    cout << "Size of int : " << sizeof(int) << endl;
+    cout << "Size of short int : " << sizeof(short int) << endl;
+    cout << "Size of long int : " << sizeof(long int) << endl;
+    cout << "Size of float : " << sizeof(float) << endl;
+    cout << "Size of double : " << sizeof(double) << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t) << endl;
+    cout << "hello world\n";
 }
