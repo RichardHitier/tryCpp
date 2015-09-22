1fdefc4101bf3bf50d873652777e7f1b251532b0
Richard Hitier <richard.hitier@co-libri.org>
2015-09-22 16:43:22 +0200

    class variable

--- rectangle.cpp
+++ rectangle.cpp
@@ -2,21 +2,36 @@
 using namespace std;
 
 class Rectangle {
+
     int width, height;
+    int myindex;
     public:
+    static int index;
     Rectangle();
     Rectangle(int, int);
+    void init();
     void setvalues(int, int);
-    void showarea(){ cout << "The area is: "<<area()<<endl;}
+    void showarea(){ cout << "The "<<myindex<< "th area is: "<<area()<<endl;}
     int area(){ return (width*height) ;}
+
 };
 
+int Rectangle::index=0;
+
+void Rectangle::init(){
+    myindex=index++;
+}
+
 Rectangle::Rectangle(){
+    this->init();
     setvalues(5,5);
 }
+
 Rectangle::Rectangle(int w, int h){
+    this->init();
     setvalues(w, h);
 }
+
 void Rectangle::setvalues(int w, int h){
     width = w;
     height = h;
