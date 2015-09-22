0e41a97d93043b6343a715b46fb3860df43b3a6f
Richard Hitier <richard.hitier@co-libri.org>
2015-09-22 15:56:43 +0200

    overload constructor

--- rectangle.cpp
+++ rectangle.cpp
@@ -4,15 +4,19 @@ using namespace std;
 class Rectangle {
     int width, height;
     public:
+    Rectangle();
     Rectangle(int, int);
     void setvalues(int, int);
+    void showarea(){ cout << "The area is: "<<area()<<endl;}
     int area(){ return (width*height) ;}
 };
 
+Rectangle::Rectangle(){
+    setvalues(5,5);
+}
 Rectangle::Rectangle(int w, int h){
     setvalues(w, h);
 }
-
 void Rectangle::setvalues(int w, int h){
     width = w;
     height = h;
@@ -20,6 +24,8 @@ void Rectangle::setvalues(int w, int h){
 
 int main(void){
     Rectangle myrect(2,3);
-    cout << "The area is: "<<myrect.area()<<endl;
+    Rectangle otherrect;
+    myrect.showarea();
+    otherrect.showarea();
     return 0;
 }
